96c5282e5dcacf7e9f5e5825cc607e7099c1c94d
Angele Williams <angele.williams5@gmail.com>
2022-06-14 09:25:10 -0600

    Fix initial render bug, add state to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,16 @@ import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
-  constructor(props) {
-    super();
+  constructor() {
+    super()
+    this.state = {
+      orders: []
+    }
   }
 
   componentDidMount() {
     getOrders()
+      .then(orderData => this.setState({orders: orderData}))
       .catch(err => console.error('Error fetching:', err));
   }
 
